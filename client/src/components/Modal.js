a9281b46a32df7e30874c8c7009f8e6f0e97db6a
Thomas Nielse <skt.thomas@gmail.com>
2020-02-19 11:04:06 +0100

    refactored modal to make it reusable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -6,11 +6,10 @@ const Modal = props => {
 return ReactDOM.createPortal( //This creates a portal that bypasses parent components to create a component almost at root level
     <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
         <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-            <div className="header">Delete Stream</div>
+            <div className="header">{props.title}</div>
             <div className="content">Are you sure you want to delete the stream?</div>
             <div className="actions">
-                <button className="ui primary button">Delete</button>
-                <button className="ui button">Cancel</button>
+                {props.actions}
             </div>
             </div>
     </div>,
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Modal from '../Modal'
 
 const StreamDelete = () => {
+
+    const actions = ( //Helper function that renders the action buttons in our modal
+        <div>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </div>
+    )
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions}/>
         </div>
     )
 }

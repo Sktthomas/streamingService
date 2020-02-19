1c159cca7e5c7f5a047d222e46b7c04fc9c288b7
Thomas Nielse <skt.thomas@gmail.com>
2020-02-19 15:56:34 +0100

    added correct routing to StreamShow. Added Switch to avoid multiple routes being shown

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route} from'react-router-dom'
+import {Router, Route, Switch} from'react-router-dom'
 import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
@@ -14,11 +14,13 @@ const App = () => {
             <Router history={createBrowserHistory}>
             <div>
             <Header /> {/* Since Header makes use of a Link component tag, we must have it inside the Router component */ }
+            <Switch>
             <Route path = "/" exact component={StreamList}/> {/*Exact ensures that we only get the exact page when using a / otherwise it would render it every time we load a page cause every one contains a / */}
             <Route path = "/streams/new" exact component={StreamCreate} />
             <Route path = "/streams/edit/:id" exact component={StreamEdit}/>
             <Route path = "/streams/delete/:id" exact component={StreamDelete}/>
-            <Route path = "/streams/show" exact component={StreamShow}/>
+            <Route path = "/streams/:id" exact component={StreamShow}/>
+            </Switch>
             </div>
             </Router>
         </div>

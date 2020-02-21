02346eef26fb5b8f34ffbbe81ec9386e158d09b1
Thomas Nielse <skt.thomas@gmail.com>
2020-02-21 12:49:42 +0100

    made sure player gets built properly and referenced

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import {connect } from 'react-redux'
 import { fetchSingleStream } from '../../actions'
-import flv from 'flv.js'
+import flv from 'flv.js' //Reaches out to grab our stream, then converts it to something our html player can play
  
 class StreamShow extends React.Component {
-
     constructor(props) {
         super(props);
 
-        this.videoRef = React.createRef(); //allows us to reference something in the DOM?!
+        this.videoRef = React.createRef(); //allows us to reference/access something in the DOM?!
     }
     componentDidMount(){
         const {id } = this.props.match.params
 
         this.props.fetchSingleStream(id);
 
+        this.buildPlayer();
+    }
+
+    buildPlayer() {
+        if(this.player || !this.props.stream){
+            return;
+        }
+
+        const {id } = this.props.match.params
+
         this.player = flv.createPlayer( {
             type: 'flv',
-            url: `http://localhost:800/live/${id}`
+            url: `http://localhost:800/live/${id}.flv`
         });
-        this.player.attachMediaElement(this.videoRef.current)
+        this.player.attachMediaElement(this.videoRef.current) //attaches the player to an element (by using React ref)
         this.player.load();
+        //we don't automatically begin playing the stream, using this.player.play()
     }
 
     render(){
@@ -32,8 +42,8 @@ class StreamShow extends React.Component {
 
         return(
             <div>
-                <video ref={this.videoRef} style={{width: '100%'}} controls={true}/>
                 <h1>{title}</h1>
+                <video ref={this.videoRef} style={{width: '100%'}} controls={true}/>
                 <h5>{description}</h5>
             </div>
         )
